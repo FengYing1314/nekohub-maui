906f05d6648f1ca453b4400fccb993c7ee054d2b
枫莹 <admin@fengying.shop>
2025-09-21 15:34:17 +0800

    Initialize .NET MAUI project with basic structure and platform-specific implementations.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "nekohub_maui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "nekohub_maui.Pages")]
